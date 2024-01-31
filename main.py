3cbf5cdfe6b352f68763c96c807a28342398ce5e
Mina Gallab <m.gallab@zequent.com>
2024-01-31 20:06:04 +0100

    update

--- main.py
+++ main.py
@@ -2,17 +2,32 @@ import i18n
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
 
 translator = i18n.Translator('localization/')
 translator.set_locale('de')
 
 #print(translator.translate('welcome'))
 
-class ZequentMavLinkApp(App):
-    def build(self):
+class LoginScreen(GridLayout):
+
+    def __init__(self, **kwargs):
+        super(LoginScreen, self).__init__(**kwargs)
+        self.cols = 1
+
         welcomeLabel = Label(text=translator.translate('welcome'))
         welcomeLabel.font_size = '50dp'
-        return welcomeLabel
+
+        connectButton = Button(text=translator.translate('connect'))
+        connectButton.font_size = '50dp'
+
+        self.add_widget(welcomeLabel)
+        self.add_widget(connectButton)
+
+class ZequentMavLinkApp(App):
+    def build(self):
+        return LoginScreen()
 
 
 if __name__ == '__main__':
