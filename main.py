c8fd47824514f339c384bf8be343425843f3673b
Mina Gallab <m.gallab@zequent.com>
2024-02-05 09:52:54 +0100

    update

--- main.py
+++ main.py
@@ -2,12 +2,20 @@ import i18n
 import kivy
 from kivymd.app import MDApp
 
-
 translator = i18n.Translator('localization/')
 translator.set_locale('de')
 
+
 class ZequentMavLinkApp(MDApp):
-    
+    def build(self):
+        self.theme_cls.primary_palette = "Orange"
+        self.theme_cls.theme_style = "Dark"
+        pass
+    def get_welcome_text(self):
+        return translator.translate('welcome')
+    def callback(self,x):
+        print(x)
+"""
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         
@@ -17,7 +25,7 @@ class ZequentMavLinkApp(MDApp):
 
     def get_welcome_text(self):
         return translator.translate('welcome')
-
+"""
 
 
 if __name__ == '__main__':
