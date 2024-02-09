1ff2f7d10334ef9eef53e8b085c6fae177d5633b
Mina Gallab <m.gallab@zequent.com>
2024-02-09 09:47:05 +0100

    update

--- main.py
+++ main.py
@@ -7,14 +7,13 @@ from kivy.clock import Clock
 from functools import partial
 import tools.customWidgets as customWidgets
 
+
 translator = i18n.Translator('tools/localization/')
 translator.set_locale('de')
 
 class ZequentMavLinkApp(MDApp):
     toolBarTitle = "MavLink"
-    connected = False
-    isMainLayoutShown = BooleanProperty(False)
-
+    ##Custom ColorPlaette
     colors = {
         "Black-primary": [0,0,0,0.9],
         "Gold-primary": [0.78,0.56,0.05,1],
@@ -23,22 +22,29 @@ class ZequentMavLinkApp(MDApp):
         "Success": [0,1,0,1],
         "Failure": [1,0,0,1],
     }
+
+    #connection status variable of return value from API
+    connected = False
     
+    #Startpoint
     def build(self):
         self.theme_cls.theme_style = "Dark"
+
+    ######Globals Start#######
     def get_welcome_text(self):
         return translator.translate('welcome')
+    
     def callback(self,x):
         print(x)
-
-    ######ZequentConnectLayout#######
-    def getConnectionStatusText(self):
-        return translator.translate('not_connected')
-    
+    ######Globals End#######
+        
+    ##Start new Root Screen
     def setNewScreen(self,*args):
         self.root.clear_widgets()
         self.root.add_widget(args[0])
 
+    ######ZequentRootLayout Start#######
+    ##Connect to Vehicle
     def tryConnection(self,button, connectionType):
         import random
         randInt = random.randint(0,1)
@@ -49,9 +55,6 @@ class ZequentMavLinkApp(MDApp):
         elif connectionType.ids.lte_button.disabled == False:
             address=connectionType.ids.lte_address
             print("LTE adress:"+str(address.text))
-
-            
-
         if randInt is 0:
             currStateLabel.text = translator.translate('failed_message')
             currStateLabel.color = self.colors["Failure"]
@@ -60,6 +63,13 @@ class ZequentMavLinkApp(MDApp):
             currStateLabel.text = translator.translate('success_message')
             currStateLabel.color = self.colors["Success"]
             Clock.schedule_once(partial(self.setNewScreen, customWidgets.MainControllerLayout()), 3)
+    ######ZequentRootLayout End#######
+
+
+    ######ZequentConnectLayout Start#######
+    def getConnectionStatusText(self):
+        return translator.translate('not_connected')
+    
     
     def openDropDown(self,topBar):
         menu_items = [
@@ -71,6 +81,7 @@ class ZequentMavLinkApp(MDApp):
         MDDropdownMenu(caller=topBar, items=menu_items).open()
     def menu_callback(self, text_item):
         print(str(text_item))
-
+    ######ZequentConnectLayout End#######
+        
 if __name__ == '__main__':
     ZequentMavLinkApp().run()
