7b3ccd38a5e76f1a73a09387b5fd6042385c9957
Mina Gallab <m.gallab@zequent.com>
2024-02-22 14:24:12 +0100

    update

---
+++ tools/py_files/appbar/zequentappbar.py
@@ -0,0 +1,64 @@
+from kivymd.uix.appbar import MDTopAppBar
+import tools.i18n as i18n
+from kivymd.uix.menu import MDDropdownMenu
+import json
+import tools.Globals as Globals
+from kivymd.app import MDApp
+
+class ZequentAppBar(MDTopAppBar):
+    #Localization
+    translator = i18n.Translator('tools/localization/')
+    app= MDApp.get_running_app()
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def open_language_dropdown(self, item):
+        menu_items = self.getLanguageDropDownItems()
+        MDDropdownMenu(caller=item, items=menu_items).open()
+
+    def getLanguageDropDownItems(self):
+        from os import walk
+
+        availableLanguages = []
+        for (dirpath, dirnames, filenames) in walk('tools/localization/'):
+            filenames = filenames
+            break
+
+        for filename in filenames:
+            filename = filename.split('.json')[0]
+            currLanguageDropDownItem = {
+                "text": filename,
+                "font_size": self.app.fontSizes['primary'],
+                "on_release": lambda language=filename: self.setLanguage(language),
+            }
+            availableLanguages.append(currLanguageDropDownItem)
+        
+        return availableLanguages
+    
+    def changeLanguageOnStart(self, topBar):
+        menu_items = self.getDropDownItemsLanguage()
+        mdDropDown = MDDropdownMenu()
+        mdDropDown.caller=topBar
+        mdDropDown.items=menu_items
+        mdDropDown.pos_hint= {'center_x':.5,'center_y':.5}
+        mdDropDown.open()
+    
+    def setLanguage(self, language):
+        ###TODO CHANGE SCREEN
+        from kivy.uix.screenmanager import ScreenManager, Screen
+        self.translator.set_locale(language)
+        self.saveInSettings(language)
+        
+    def saveInSettings(self, language):
+        with open(Globals.settingsFile) as infile:
+            data = json.load(infile)
+        data["lastUsedLanguage"] = language
+        with open(Globals.settingsFile, 'w') as outfile:
+            json.dump(data, outfile)
+    
+    def callback(self,x):
+        print(x)
+    
+    def menu_callback(self, text_item):
+        print(str(text_item))
--- tools/py_files/layouts/zequentrootlayout.py
+++ tools/py_files/layouts/zequentrootlayout.py
@@ -1,8 +1,5 @@
 from kivy.uix.boxlayout import BoxLayout
 import tools.i18n as i18n
-from kivymd.uix.menu import MDDropdownMenu
-import json
-import tools.Globals as Globals
 from kivymd.app import MDApp
 
 class ZequentRootLayout(BoxLayout):
@@ -24,55 +21,7 @@ class ZequentRootLayout(BoxLayout):
     def build(self):
         pass
 
-    def open_language_dropdown(self, item):
-        menu_items = self.getLanguageDropDownItems()
-        MDDropdownMenu(caller=item, items=menu_items).open()
-
-    def getLanguageDropDownItems(self):
-        from os import walk
-
-        availableLanguages = []
-        for (dirpath, dirnames, filenames) in walk('tools/localization/'):
-            filenames = filenames
-            break
-
-        for filename in filenames:
-            filename = filename.split('.json')[0]
-            currLanguageDropDownItem = {
-                "text": filename,
-                "font_size": self.app.fontSizes['primary'],
-                "on_release": lambda language=filename: self.setLanguage(language),
-            }
-            availableLanguages.append(currLanguageDropDownItem)
-        
-        return availableLanguages
-    
-    def changeLanguageOnStart(self, topBar):
-        menu_items = self.getDropDownItemsLanguage()
-        mdDropDown = MDDropdownMenu()
-        mdDropDown.caller=topBar
-        mdDropDown.items=menu_items
-        mdDropDown.pos_hint= {'center_x':.5,'center_y':.5}
-        mdDropDown.open()
-    
-    def setLanguage(self, language):
-        ###TODO CHANGE SCREEN
-        from kivy.uix.screenmanager import ScreenManager, Screen
-        self.translator.set_locale(language)
-        self.saveInSettings(language)
-        
-    def saveInSettings(self, language):
-        with open(Globals.settingsFile) as infile:
-            data = json.load(infile)
-        data["lastUsedLanguage"] = language
-        with open(Globals.settingsFile, 'w') as outfile:
-            json.dump(data, outfile)
-    
-    def callback(self,x):
-        print(x)
-    
-    def menu_callback(self, text_item):
-        print(str(text_item))
+   
     
    
 
