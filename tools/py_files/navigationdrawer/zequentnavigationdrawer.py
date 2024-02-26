fbab28d5dc2dbb361bf0c3981ac0486495adb63f
Mina Gallab <m.gallab@zequent.com>
2024-02-26 19:27:51 +0100

    update

--- tools/py_files/navigationdrawer/zequentnavigationdrawer.py
+++ tools/py_files/navigationdrawer/zequentnavigationdrawer.py
@@ -5,18 +5,18 @@ from kivymd.app import MDApp
 
 
 class ZequentNavigationDrawer(MDNavigationDrawer):
-    i = 0
+    isCreated = False
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         
     def on_state(self, widget, state):
-        if self.i == 0:
+        if not self.isCreated:
             self.app=MDApp.get_running_app()
             navigationItems = Utils.getNavigationDrawerItems(self.app.root.ids.translator, self.app.root.ids.sm)
             
             for item in navigationItems:
                 self.ids.drawer_menu.add_widget(item)
-            self.i=1
+            self.isCreated=True
             
                 
